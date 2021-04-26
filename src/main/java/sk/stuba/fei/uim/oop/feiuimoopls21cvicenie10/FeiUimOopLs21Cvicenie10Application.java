972d68c44afe74f9daab98fedfcdf4fca01fb9be
Jakub Kovář <jakub.kovar@stuba.sk>
2021-04-26 13:17:34 +0200

    add files to git

---
+++ src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/FeiUimOopLs21Cvicenie10Application.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiUimOopLs21Cvicenie10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeiUimOopLs21Cvicenie10Application.class, args);
+    }
+
+}
---
+++ src/test/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/FeiUimOopLs21Cvicenie10ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiUimOopLs21Cvicenie10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
