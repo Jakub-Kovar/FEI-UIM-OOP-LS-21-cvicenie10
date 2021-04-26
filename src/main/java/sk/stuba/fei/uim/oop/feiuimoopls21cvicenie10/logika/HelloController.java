c52f2b8050c2cb2b4a70459270bd67460a1523c6
Jakub Kovář <jakub.kovar@stuba.sk>
2021-04-26 14:58:53 +0200

    return object

--- src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
+++ src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
@@ -1,6 +1,8 @@
 package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10.logika;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +17,9 @@ public class HelloController {
         this.service = service;
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello world " + (this.counter++);
+    @GetMapping(value = "/hello")
+    public HelloResponse hello() {
+        return new HelloResponse(this.counter++, "hello world");
     }
 
     @GetMapping("/param")
---
+++ src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloResponse.java
@@ -0,0 +1,10 @@
+package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10.logika;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+public class HelloResponse {
+    private int number;
+    private String message;
+}
