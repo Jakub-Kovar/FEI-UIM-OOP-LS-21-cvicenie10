7cd3f3fb58a8621b03afee478d1841df62b1e233
Jakub Kovář <jakub.kovar@stuba.sk>
2021-04-26 14:02:37 +0200

    request param

--- src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
+++ src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
@@ -1,13 +1,21 @@
 package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10.logika;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private int counter = 0;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello world";
+        return "hello world " + (this.counter++);
+    }
+
+    @GetMapping("/param")
+    public String helloParam(@RequestParam(name = "name", defaultValue = "world") String name) {
+        return "hello " + name;
     }
 }
