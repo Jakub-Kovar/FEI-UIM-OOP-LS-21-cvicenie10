8f3a054085cf2f31d28ba9c49430e55e8931615a
Jakub Kovář <jakub.kovar@stuba.sk>
2021-04-26 15:01:46 +0200

    removed unused imports

--- src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
+++ src/main/java/sk/stuba/fei/uim/oop/feiuimoopls21cvicenie10/logika/HelloController.java
@@ -1,8 +1,6 @@
 package sk.stuba.fei.uim.oop.feiuimoopls21cvicenie10.logika;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
